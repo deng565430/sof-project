aa396357298a7ffabfb287208a365254c39d305f
deng565430 <562340148@qq.com>
2017-05-10 19:27:48 +0800

    增加登录模块

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="header">
      <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1"><router-link to="/">SOF</router-link></el-menu-item>
-      <el-menu-item index="2"><router-link to="/index">主页</router-link></el-menu-item>
-      <el-menu-item index="3"><router-link to="/client">客户下单系统</router-link></el-menu-item>
-      <el-menu-item index="4"><router-link to="/phone">电话管理</router-link></el-menu-item>
-      <el-menu-item index="5"><router-link to="/analysis">分析报告</router-link></el-menu-item>
-      <el-menu-item index="6"><router-link to="/management">客户管理</router-link></el-menu-item>
+      <el-menu-item index="1"><router-link class="itemWdith" to="/">SOF</router-link></el-menu-item>
+      <el-menu-item index="2"><router-link class="itemWdith" to="/index">主页</router-link></el-menu-item>
+      <el-menu-item index="3"><router-link class="itemWdith" to="/client">客户下单系统</router-link></el-menu-item>
+      <el-menu-item index="4"><router-link class="itemWdith" to="/phone">电话管理</router-link></el-menu-item>
+      <el-menu-item index="5"><router-link class="itemWdith" to="/analysis">分析报告</router-link></el-menu-item>
+      <el-menu-item index="6"><router-link class="itemWdith" to="/management">客户管理</router-link></el-menu-item>
       <el-menu-item index="7"><a href="javascript:;"  target="_blank" @click="submitForm">客户管理</a></el-menu-item>
       <el-menu-item index="9" style="float:right"><a href="javascript:;"  target="_blank">注册</a></el-menu-item>
       <el-menu-item index="8" style="float:right" @click="login"><router-link to="">登录</router-link></el-menu-item>
@@ -47,6 +47,9 @@ export default {
 
 </script>
 
-<style lang="css" scoped>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  .itemWdith
+    display: inline-block
+    width: 100%
+    height: 100%
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,24 @@
 <template>
-<div>
+<div id="login">
 
-  <el-dialog title="提示" v-model="unLogin" size="tiny">
-    <span>这是一段信息</span>
-    <span slot="footer" class="dialog-footer">
-      <el-button @click="unLogin ">取 消</el-button>
-      <el-button type="primary" @click="unLogin">确 定</el-button>
+  <el-dialog title="登录" :visible.sync="dialogVisible" size="tiny" :before-close="handleClose">
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="用户名" prop="age">
+      <el-input v-model.number="ruleForm2.age"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+    </el-form-item>
+ 
+    <span>
+      <el-button type="primary" @click="submitForm('ruleForm2')">确 定</el-button>
+      <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      <el-button @click="unLogin">取 消</el-button>
     </span>
-  </el-dialog>
+   </el-form>
+</el-dialog>
 
 </div>
 </template>
@@ -18,13 +29,63 @@ export default {
   name: 'login',
 
   data () {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('用户名不能为空'));
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleForm2.checkPass !== '') {
+          this.$refs.ruleForm2.validateField('checkPass');
+        }
+        callback();
+      }
+    };
     return {
-      dialogVisible: this.$store.state.dialogVisible
+      dialogVisible: this.$store.state.dialogVisible,
+      ruleForm2: {
+        pass: '',
+        checkPass: '',
+        age: ''
+      },
+      rules2: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        age: [
+          { validator: checkAge, trigger: 'blur' }
+        ]
+      }
     };
   },
   methods: {
     unLogin () {
-      this.$store.state.dialogVisible = !this.$store.state.dialogVisible;
+      this.dialogVisible = this.$store.state.dialogVisible = !this.$store.state.dialogVisible;
+    },
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+          this.$store.state.dialogVisible = false;
+        })
+        .catch(_ => {});
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+          this.dialogVisible = this.$store.state.dialogVisible = !this.$store.state.dialogVisible;
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
