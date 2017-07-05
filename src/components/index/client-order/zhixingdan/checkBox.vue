ac1cf411aa27a43dd96a104d3506bcf7524f066f
deng565430 <562340148@qq.com>
2017-07-05 19:12:17 +0800

    修改执行单逻辑

---
+++ src/components/index/client-order/zhixingdan/checkBox.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <el-checkbox v-model="checked" v-for="(val, index) in data" @change="change" :label="val.code" :name="val.name">{{val.name}}</el-checkbox>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'checkBox',
+
+  data () {
+    return {
+      checked: []
+    };
+  },
+  props: ['data'],
+  methods: {
+    change (e) {
+      const checked = e.target.checked;
+      const value = e.target.value;
+      this.$emit('selectData', {checked, value});
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
